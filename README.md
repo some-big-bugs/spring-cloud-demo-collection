6898d51ab31f33bd66efcf8ce2ba1d8cbef86a26
zhangyunan <zyndev@gmail.com>
2021-04-18 19:20:43 +0800

    feat: add user server demo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-cloud-demo-collection
+
 Spring Cloud Demo Collection
+
+Spring Cloud 常用组件集合
---
+++ user/user-server/src/test/java/com/sbb/scdc/user/UserServerApplicationTest.java
@@ -0,0 +1,14 @@
+package com.sbb.scdc.user;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class UserServerApplicationTest {
+
+  @Test
+  public void contextLoads() {
+  }
+}
